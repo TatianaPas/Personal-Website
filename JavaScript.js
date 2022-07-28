fbeeaef657eae593f0131891d6e1ed6927e24868
Tatiana Pasechnik <81549043+TatianaPas@users.noreply.github.com>
2022-07-28 20:54:32 +1200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,16 +1,16 @@
-﻿const links = document.querySelectorAll('.link');
+const links = document.querySelectorAll('.link');
 
 links.forEach(link => {
-    link.addEventListener('click', () => {
-        links.forEach(ele => ele.classList.remove('active'));
-        link.classList.add('active');
-    })
+  link.addEventListener('click', () => {
+    links.forEach(ele => ele.classList.remove('active'));
+    link.classList.add('active');
+  })
 })
 
 const toggleBtn = document.querySelector('.toggle-btn');
 const linksContainer = document.querySelector('.links-container');
 
 toggleBtn.addEventListener('click', () => {
-    toggleBtn.classList.toggle('active');
-    linksContainer.classList.toggle('show');
+  toggleBtn.classList.toggle('active');
+  linksContainer.classList.toggle('show');
 })
